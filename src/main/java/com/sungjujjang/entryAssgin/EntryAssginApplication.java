98a619767593c2f4dc7f0ce6418ade3543227cfa
JANG SEONG JU <printhelloworld0702@gmail.com>
2026-08-19 13:13:23 +0900

    first commit

---
+++ src/main/java/com/sungjujjang/entryAssgin/EntryAssginApplication.java
@@ -0,0 +1,13 @@
+package com.sungjujjang.entryAssgin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntryAssginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntryAssginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sungjujjang/entryAssgin/EntryAssginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungjujjang.entryAssgin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntryAssginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
